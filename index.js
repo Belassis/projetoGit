59e82e9d176eced76380dbe2d356164abde470ce
unknown <isabelly.assis.5261@gmail.com>
2025-03-11 15:40:41 -0300

    Código de boas vindas

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log("Olá mundo!");
+//a variável nome vai servir para que o usuário coloque seu nome.
+
+
+let nome = String(prompt("Olá, qual seu nome?")); 
+console.log("Seja bem vindo(a), "+nome); 
